7d9f680548b4ebd585475418aaead11613877981
LAPTOP-58QGQQLE\David Private <jonid89@gmail.com>
2023-09-03 13:38:59 +0200

    Iterate through Mineshafts to Save and Load
    Iterate through Mineshafts to Save and Load

--- Assets/_Scripts/GameCode/GameProgress/GameProgress.cs
+++ Assets/_Scripts/GameCode/GameProgress/GameProgress.cs
@@ -21,7 +21,7 @@ public class GameProgress
 
     private int _mineShaftsCount;
     private List<int> _mineShaftsPerMine = new List<int>();
-    private List<List<int>> _minesLevels = new List<List<int>>();
+    private List<List<int>> _minesMineShaftsLevels = new List<List<int>>();
     //private int[] _mineShaft1Levels = new int[] {1,1};
     private int _mineShaft1Level;
 
@@ -33,17 +33,6 @@ public class GameProgress
         _mineshaftCollectionModel = mineshaftCollectionModel;
         _disposable = disposable;
         
-        //SubscribeToMineModels();
-    }
-
-    private void SubscribeToMineModels()
-    {            
-        /*_elevatorModel.Level.Subscribe(level => _elevatorLevel = level)
-            .AddTo(_disposable);
-
-        _mineshaftCollectionModel.GetModel(1).Level.Subscribe(level => _mineShaft1Level = level)
-            .AddTo(_disposable);*/
-
     }
 
     public void SwitchToMine(int switchMineID)
@@ -59,12 +48,18 @@ public class GameProgress
 
     private void SaveMine()
     {
+        //Saving Elevator Level
         _elevatorLevels[_currentMineID] = _elevatorModel.Level.Value;
 
-        for (int i = 1; i == _mineshaftCollectionModel.GetCount(); i++)
+        //Iterate through List of mineshafts on Current Mine to save Active mineshafts Level
+        for (int i = 1; i <= _mineshaftCollectionModel.GetCount(); i++)
         {
-            int _mineShaftLevelSave = _mineshaftCollectionModel.GetModel(i).Level.Value;
-            _minesLevels[_currentMineID].Insert(i, _mineShaftLevelSave);
+            if(_mineshaftCollectionModel.GetView(i).gameObject.activeSelf)
+            {
+                int _mineShaftLevelSave = _mineshaftCollectionModel.GetModel(i).Level.Value;
+                _minesMineShaftsLevels[_currentMineID].Insert(i-1, _mineShaftLevelSave);
+                _mineShaftsPerMine[_currentMineID] = i;                
+            }
         }
     }
 
@@ -73,20 +68,34 @@ public class GameProgress
         _elevatorLevel = _elevatorLevels[_currentMineID];
         _elevatorModel.MineSwitch(_elevatorLevel);
         
-        for (int i = 1; i == _mineshaftCollectionModel.GetCount(); i++)
+        for (int i = 1; i <= _mineshaftCollectionModel.GetCount(); i++)
         {
-            int _mineShaftLevelLoad = _minesLevels[_currentMineID][0];
-            _mineshaftCollectionModel.GetModel(1).MineSwitch(_mineShaftLevelLoad);
+            if(i <= _mineShaftsPerMine[_currentMineID])
+            {
+                _mineshaftCollectionModel.GetView(i).gameObject.SetActive(true);
+                int _mineShaftLevelLoad = _minesMineShaftsLevels[_currentMineID][i-1];
+                Debug.Log("_mineShaftLevelLoad: " + _mineShaftLevelLoad);
+                _mineshaftCollectionModel.GetModel(i).MineSwitch(_mineShaftLevelLoad);
+                if(i == _mineShaftsPerMine[_currentMineID])
+                {
+                    _mineshaftCollectionModel.GetView(i).NextShaftView.gameObject.SetActive(true);
+                }
+            }
+            else{
+                _mineshaftCollectionModel.GetModel(i).MineSwitch(1);
+                _mineshaftCollectionModel.GetView(i).gameObject.SetActive(false);
+            }
         }
     }
     
     private void InitializeMineLevelsList()
     {
-        // Initialize _minesLevels with 1 mineshaft on level 1 for each mine
+        // Initialize _minesMineShaftsLevels with 1 mineshaft on level 1 for each mine
         for (int i = 0; i < _numberOfMines; i++)
         {
-            _minesLevels.Add(new List<int>());
-            _minesLevels[i].Add(1);
+            _mineShaftsPerMine.Insert(i, 1);
+            _minesMineShaftsLevels.Add(new List<int>());
+            _minesMineShaftsLevels[i].Add(1);
         }
     }
     
